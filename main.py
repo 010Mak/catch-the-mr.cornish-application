103bd1238e70af2e8652aad2139253870d4314dd
010Mak <96219805+010Mak@users.noreply.github.com>
2023-12-01 09:31:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ pygame.init()
 
 # Constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
-FALLING_SPEED_INCREMENT = 1
+FALLING_SPEED_INCREMENT = 0.1  # Gravity effect
 FALLING_ITEM_SIZE = 50
-BASKET_SIZE = 100
+BASKET_SIZE = 60  # Smaller basket size
 
 # Colors
 WHITE = (255, 255, 255)
@@ -44,23 +44,25 @@ while running:
 
     # Movement
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and basket_x > 0:
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and basket_x > 0:
         basket_x -= 5
-    if keys[pygame.K_RIGHT] and basket_x < SCREEN_WIDTH - BASKET_SIZE:
+    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and basket_x < SCREEN_WIDTH - BASKET_SIZE:
         basket_x += 5
 
-    # Update falling item
+    # Update falling item with gravity effect
     falling_item_y += falling_speed
+    falling_speed += FALLING_SPEED_INCREMENT
     if falling_item_y > SCREEN_HEIGHT:
         falling_item_y = -FALLING_ITEM_SIZE
+        falling_speed = 5
         falling_item_x = random.randint(0, SCREEN_WIDTH - FALLING_ITEM_SIZE)
 
     # Check collision
     if (falling_item_y + FALLING_ITEM_SIZE > SCREEN_HEIGHT - BASKET_SIZE and
-        basket_x < falling_item_x < basket_x + BASKET_SIZE):
+        basket_x < falling_item_x + FALLING_ITEM_SIZE / 2 < basket_x + BASKET_SIZE):
         score += 1
-        falling_speed += FALLING_SPEED_INCREMENT
         falling_item_y = -FALLING_ITEM_SIZE
+        falling_speed = 5
         falling_item_x = random.randint(0, SCREEN_WIDTH - FALLING_ITEM_SIZE)
 
     # Draw
